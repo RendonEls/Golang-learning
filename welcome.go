7a2c7353326a342d199cacdaf4900dfff479f62f
Rendon Elsesser <rendonelsesser@gmail.com>
2019-02-25 11:55:58 -0500

    single line var dec

--- welcome.go
+++ welcome.go
@@ -8,11 +8,9 @@ import (
 
 // calculate the mean of two numbers 
 func main() {
-	var x float64  //2 types of floats, 32 and 64
-	var y float64
 
-	x = 1
-	y = 2
+	x := 1.0
+	y := 2.0
 
 	fmt.Printf("x=%v, type of %T\n", x, x)
 	//fmt.Printf function gets a template to print and a value to fill the function
@@ -20,10 +18,8 @@ func main() {
 	//%t verb prints it's type
 	fmt.Printf("y=%v, type of %T\n", y, y)
 
-		//define mean
-	var mean float64
 		//logic to calculate mean
-	mean = (x + y) / 2.0
+	mean := (x + y) / 2.0
 		//template to print and it's value/types
 	
 	fmt.Printf("result: %v, type of %T\n", mean, mean)
