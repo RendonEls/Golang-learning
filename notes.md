da4e4c54f55d4f901e6bec6ca92b88c6f04d982a
Rendon Elsesser <rendonelsesser@gmail.com>
2019-02-25 11:54:16 -0500

    finding average of 2 numbers with multi-line var declarations

---
+++ notes.md
@@ -0,0 +1,7 @@
+#What is Go? 
+
+
+#Cool stuff
+1 - built in test suite which can also run benchmarks to test the performance of code
+2 - get - installs packages and dependencies
+3 - $fmt - is a code formatter which is built-in to go
--- welcome.go
+++ welcome.go
@@ -1,4 +1,4 @@
-// Print a friendly greeting
+
 
 package main
 
@@ -6,6 +6,25 @@ import (
 	"fmt"
 )
 
+// calculate the mean of two numbers 
 func main() {
-	fmt.Println("Welcome Gophers ☺")
+	var x float64  //2 types of floats, 32 and 64
+	var y float64
+
+	x = 1
+	y = 2
+
+	fmt.Printf("x=%v, type of %T\n", x, x)
+	//fmt.Printf function gets a template to print and a value to fill the function
+	//%v verb prints a go object
+	//%t verb prints it's type
+	fmt.Printf("y=%v, type of %T\n", y, y)
+
+		//define mean
+	var mean float64
+		//logic to calculate mean
+	mean = (x + y) / 2.0
+		//template to print and it's value/types
+	
+	fmt.Printf("result: %v, type of %T\n", mean, mean)
 }
